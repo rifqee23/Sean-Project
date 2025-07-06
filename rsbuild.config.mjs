f4305ab2f1a16aa9fb86e1489d56a2418042b0a3
rifqee23 <rifqifebrianto746@gmail.com>
2025-07-06 23:57:35 +0700

    fix: add missing dependencies

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -1,6 +1,17 @@
-import { defineConfig } from '@rsbuild/core';
-import { pluginReact } from '@rsbuild/plugin-react';
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    postcss: (_opt, { addPlugins }) => {
+      addPlugins("@tailwindcss/postcss");
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 });
